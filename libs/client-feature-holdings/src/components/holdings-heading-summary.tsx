f9f2caac3c18dee4ec825bcb1e1e38491fcabc83
Steven Moseley <avington12345@gmail.com>
2025-07-21 13:55:35 -0400

    feat: remove fake portfolio data from HoldingBreadcrumbs; add HoldingsHeadingSummary component for portfolio metrics display

---
+++ libs/client-feature-holdings/src/components/holdings-heading-summary.tsx
@@ -0,0 +1,29 @@
+import { HeadingContainer } from '@adg/client-components';
+import { StyledHoldingsHeadingSummary } from './styled-holdings-heading-sumamry';
+
+export const HoldingsHeadingSummary: React.FC = () => {
+  return (
+    <StyledHoldingsHeadingSummary>
+      <HeadingContainer
+        label="Portfolio Value"
+        value={100000}
+        showDollar={true}
+      />
+      <HeadingContainer label="Cost" value={80000} showDollar={true} />
+      <HeadingContainer
+        label="Total Gains"
+        value={20000}
+        percentage={25}
+        showDollar={true}
+        colorize={true}
+      />
+      <HeadingContainer
+        label="Daily Gains"
+        value={500}
+        percentage={0.5}
+        showDollar={true}
+        colorize={true}
+      />
+    </StyledHoldingsHeadingSummary>
+  );
+};
