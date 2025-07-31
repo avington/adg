b4f34a5b516c1bc0413193c3ea1748105eec31b7
Steve Moseley <avington12345@gmail.com>
2025-07-31 16:41:04 -0400

    feat: update position overview collection name; add holdings overview hooks and queries; implement HoldingsOverviewTable component (#35)
    * feat: update position overview collection name; add holdings overview hooks and queries; implement HoldingsOverviewTable component
    
    * feat: remove POSITION_OVERVIEW_BY_PORTFOLIO query to streamline data fetching

---
+++ libs/client-graphql-data/src/hooks/use-holdings-overview.tsx
@@ -0,0 +1,39 @@
+import { useQuery, QueryResult } from '@apollo/client';
+import { PositionOverviewProjection } from '@adg/server-domain-read-models';
+import {
+  POSITION_OVERVIEWS,
+  POSITION_OVERVIEW,
+} from '../queries/position-overview';
+
+interface PositionOverviewsData {
+  positionOverviews: PositionOverviewProjection[];
+}
+
+interface PositionOverviewData {
+  positionOverview: PositionOverviewProjection | null;
+}
+
+export const usePositionOverviews = (
+  portfolioId: string
+): QueryResult<PositionOverviewsData> => {
+  return useQuery<PositionOverviewsData>(POSITION_OVERVIEWS, {
+    variables: { portfolioId },
+    skip: !portfolioId,
+  });
+};
+
+export const usePositionOverview = (
+  positionId: string
+): QueryResult<PositionOverviewData> => {
+  return useQuery<PositionOverviewData>(POSITION_OVERVIEW, {
+    variables: { positionId },
+    skip: !positionId,
+  });
+};
+
+// Convenience hook with better naming for holdings context
+export const useHoldingsOverview = (
+  portfolioId: string
+): QueryResult<PositionOverviewsData> => {
+  return usePositionOverviews(portfolioId);
+};
